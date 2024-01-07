571fb258d5f32a3730169325766cddabc04ead27
Jordan Forbes <jordforb@gmail.com>
2024-01-07 17:10:34 -0500

    created tilemovement which does not work

---
+++ Player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var currPos = [0,0]
+
+func _input(event):
+	if event.is_action_pressed("ui_right"):
+		currPos[0] += 32
+		get_node("Player").look_at(self.position + Vector2(1,0))
+	elif event.is_action_pressed("ui_left"):
+		currPos[0] -= 32
+		get_node("Player").look_at(self.position +Vector2(-1,0))
+	elif event.is_action_pressed("ui_up"):
+		currPos[1] -=32
+		get_node("Player").look_at(self.position + Vector2(0,-1))
+	elif event.is_action_pressed("ui_down"):
+		currPos[1] += 32
+		get_node("Player").look_at(self.position + Vector2(0,1))
+
+	self.position = Vector2(currPos[0], currPos[1])
